7dd47eed983439adb0a022ad3a8e7d5f12405db2
root <root@mostafa-pc>
2023-11-14 23:43:16 +0200

    add FormsModule and logic to login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface LoginModel {
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  model: LoginModel = {
+    email: '',
+    password: ''
+  };
+  
+  onSubmit() {
+    // Handle form submission logic here
+    console.log('Form submitted:', this.model);
+  }
 
 }
