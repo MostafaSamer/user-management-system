afd7c1ccb6a658c1a85f8c9e6e613ee79ae51f8a
root <root@mostafa-pc>
2023-11-15 10:55:21 +0200

    add loggedin user in auth services

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
-
-interface LoginModel {
-  username: string;
-  password: string;
-}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  errorMessage: string | null = null;
 
   constructor(
     private authService: AuthService,
@@ -26,13 +22,29 @@ export class LoginComponent {
       password: ['', Validators.required]
     });
   }
-  
+
+  ngOnInit() {
+    // Check if the user is already logged in when the component initializes
+    if (this.authService.getLoggedInUser()) {
+      this.router.navigate(['/']);
+    }
+  }
+
   onSubmit() {
     if (this.loginForm?.invalid) return;
     const { username, password } = this.loginForm.value;
-      this.authService.login(username, password).subscribe((res) => {
-        this.router.navigate(['/']);
-      });
+    this.authService.login(username, password).subscribe(
+      (res) => {
+        if (res) {
+          this.router.navigate(['/']);
+        } else {
+          this.errorMessage = 'Invalid username or password.';
+        }
+      },
+      (error) => {
+        console.error('An error occurred during login:', error);
+        this.errorMessage = 'An unexpected error occurred.';
+      }
+    );
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,10 +6,24 @@ import { Observable, of } from 'rxjs';
 })
 export class AuthService {
 
-  constructor() { }
-  
+  private static readonly ADMIN_CREDENTIALS = { username: 'admin', password: 'admin123' };
+  private static readonly USER_CREDENTIALS = { username: 'user', password: 'user123' };
+  private loggedInUser: { username: string, role: string } | null = null;
+
   login(username: string, password: string): Observable<boolean> {
-    // Replace the following with your actual authentication logic or API call
-    return of(true);
+    if (username === AuthService.ADMIN_CREDENTIALS.username && password === AuthService.ADMIN_CREDENTIALS.password) {
+      this.loggedInUser = { username, role: 'admin' };
+      return of(true);
+    } else if (username === AuthService.USER_CREDENTIALS.username && password === AuthService.USER_CREDENTIALS.password) {
+      this.loggedInUser = { username, role: 'user' };
+      return of(true);
+    } else {
+      this.loggedInUser = null;
+      return of(false);
+    }
+  }
+  
+  getLoggedInUser(): { username: string, role: string } | null {
+    return this.loggedInUser;
   }
 }
